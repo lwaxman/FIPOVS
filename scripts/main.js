d8f7bcf56073cac20cab50b802bf636ac77719a3
Laurie Waxman <laurie.waxman@gmail.com>
2014-05-12 02:39:26 -0400

    fixed background

--- scripts/main.js
+++ scripts/main.js
@@ -1,64 +1,69 @@
 $(document).ready(function(){
 
-	// (function titleScroller(text) {
-	// 	document.title = text;
-	// 	console.log(text);
-	// 	setTimeout(function () {
-	// 		titleScroller(text.substr(1) + text.substr(0, 1));
-	// 	}, 200);
-	// }("Text to scroll goes HEEEEEEEEEEEEEEEEEEERE. "));
+	// var backgroundColour = '#FFF';
 
 	runPageFour();
-	$('#pageOne').hide();
-	$('#pageTwo').hide();
+
 	$('#pageThree').hide();
+	$('#pageTwo').hide();
+	$('#pageOne').hide();
 	$('#pageFive').hide();
 
 	var phoneBuzzInterval;
+	var popUpInterval;
 
 	function runPageOne(){
+		titleScroller('~~~~~~F.O.P.I.V.S.~~~~~~');
+		setBackground('#004604');
 		$('#pageOne').show();
+		$('#agreeOne').hide();
 		$('#agreeTwo').hide();
 		$('#agreeThree').hide();
 		$('#agreeFour').hide();
 		$('#agreeFive').hide();
 		$('#agreeSix').hide();
-		$('.text').hide();
+		// $('.text').hide();
 		$('.rushkoff').hide();
 
-		$('#agreeOne').on('click', function(){
-			$('#agreeTwo').show();
-		});
+		var count = 1;
+		var idCount = 1;
+		var delay = 500;
 
-		$('#agreeTwo').on('click', function(){
-			$('#agreeThree').show();
-		});
+		popUpInterval = setInterval(function(){
+			$('#container').append('<img src="assets/images/agreement'+count+'.png" class="image" id="agree'+idCount+'" alt="agree">');
+			$('#agree'+idCount).css({
+				'position':'absolute',
+				'left': Math.random()*60 + '%',
+				'top': Math.random()*80 + '%'
+			});
+			count++;
+			idCount++;
+			if(count>6) count=1;
+			if(idCount>10) clearInterval(popUpInterval);
 
-		$('#agreeThree').on('click', function(){
-			$('#agreeFour').show();
-		});
-		$('#agreeFour').on('click', function(){
-			$('#agreeFive').show();
-		});
-		$('#agreeFive').on('click', function(){
-			$('#agreeSix').show();
-		});
-		$('#agreeSix').on('click', function(){
-			$('#container').children().remove();
-			$('.text').show();
-		});
+			$('.image').click(function(){ 
+				$('#'+this.id).remove();
+				if( $('#container').is(':empty') ){
+					$('#container').remove();
+				}
+			});
+		},delay);
+
 		$('.more').on('click', function(){
 			$('.text').remove();
 			$('.rushkoff').show();
 		});		
 
 		$('.next').on('click', function(){
+			$('#pageOne').remove();
 			runPageTwo();
 		});
 	}
 
 	function runPageTwo(){
-		$('#pageOne').hide();
+		// backgroundColour = '#1A2881';
+		// $()
+		setBackground('#1A2881');
 		$('#pageTwo').show();
 		$('body').css('background','#0B0F46');
 		// $('.text').hide();
@@ -87,11 +92,14 @@ $(document).ready(function(){
 	}
 
 	function runPageThree(){
+		setBackground('#6D2817');
 		$('#pageTwo').hide();
 		$('#pageThree').show();
 		$('body').css('background','#500D06');
 		$('.text').hide();
 		$('.rushkoff').hide();
+		$('#phoneScreen img').hide();
+		$('.more').hide();
 
 		$('#phoneBase').jrumble();
 		$('#phoneSlider').slider({
@@ -108,7 +116,6 @@ $(document).ready(function(){
 			//on slide...
 			$('#phoneSlider').on('slide', function(event, ui){
 				var position = $('#phoneSlider').slider('value');
-				console.log(position);
 
 				//if the slider position is full
 				if(position>=90){
@@ -116,8 +123,19 @@ $(document).ready(function(){
 					setTimeout(function(){
 						$('#phoneSlider').remove();
 						$('#phoneSliderBG').remove();
-						$('#phoneScreen').css({'background':'#FFF', 'box-shadow':'0px 0px 40px 10px #7D7D7D'});
+						$('#phoneScreen img').show();
+						$('#phoneScreen').css({'background':'white', 'box-shadow':'0px 0px 40px 10px #7D7D7D'});
 						clearInterval(phoneBuzzInterval);
+						$('.more').show();
+
+						$('.more').on('click', function(){
+							$('#container3').remove();
+							$('.rushkoff').show();
+						});
+
+						$('.next').on('click', function(){
+							runPageFour();
+						});
 					},200);
 				}
 			});
@@ -133,26 +151,26 @@ $(document).ready(function(){
 		}, 2000);	
 	}//end page 3
 
-
+	var delay;
 	function runPageFour(){
 		titleScroller('jamina-mina ey eyyy walka walk eyy eyy');
-		setBackground('#064959');
+		setBackground('#085A5F');
 		$('#pageThree').hide();
 		$('#pageFour').show();
-		$('body').css('background','#065469');
+		$('body').css('background','#064550');
 		$('.text').hide();
 		$('.rushkoff').hide();
 
 		// console.log(result);
 
-
+		delay = Math.random()*5000;
 		setInterval(function(){
 			$('.blurryText').css({'text-shadow':'0px 0px 10px #000'});
 			setTimeout(function(){
 				$('.blurryText').css({'text-shadow':'0px 0px 0px #000'});
 				
 			},200);
-		},Math.random()*5000);
+		},delay);
 	}
 
 	function titleScroller(text){
@@ -162,32 +180,59 @@ $(document).ready(function(){
 		}, 200);
 	}
 
+	var text;
+	var result;
+	var newResult;
+	var width; 
+
 	function setBackground(textColour){
-		var text = "In our society, networked technologies are everywhere. Everyone is online all the time, through cell phones, computers, or even smart watches. In this digital age, I find myself feeling rather analog. By this I mean to say, I am aware of how much time I spend online, and I make a conscious decision to spend time offline. I read books and take notes on paper; I turn off my phone when I got to sleep; I turn off my computer when I don't plan on using it; My social media presence is quite low; I sketch out ideas for websites and other programming projects in physical sketchbooks before starting to work. Yet, even still, I am compelled to check my phone and my email on a regular basis, as if they control me, and not the other way around. For this reason, I decided to explore the ways in which we interact with technology in our daily lives.";
-		var result = text.split("");
-		var width = $(document).width();
-		console.log(width);
+		$('.newDiv').remove();
+		$('.wrapper').remove();
 
-		$('body').append('<div id="wrapper"></div>');
+		text = "In our society, networked technologies are everywhere. Everyone is online all the time, through cell phones, computers, or even smart watches. In this digital age, I find myself feeling rather analog. By this I mean to say, I am aware of how much time I spend online, and I make a conscious decision to spend time offline. I read books and take notes on paper; I turn off my phone when I got to sleep; I turn off my computer when I don't plan on using it; My social media presence is quite low; I sketch out ideas for websites and other programming projects in physical sketchbooks before starting to work. Yet, even still, I am compelled to check my phone and my email on a regular basis, as if they control me, and not the other way around. For this reason, I decided to explore the ways in which we interact with technology in our daily lives.";
+		result = text.split("");
+		result.sort();
+		result = result.join("");
+		result = result.trim();
 
-		var repeatBG = width/100;
+
+		width = $(document).width();
+
+		$('body').append('<div class="wrapper"></div>');
+
+		var repeatBG = Math.ceil(width/95);
 		for(var i=0; i<repeatBG; i++){
-			$('#wrapper').append('<div class="newDiv"></div>');
+			$('.wrapper').append('<div class="newDiv"></div>');
 			$('.newDiv:nth-child('+(i+1)+')').css({
 				'width':'100px', 
 				'word-wrap':'break-word', 
 				'display':'inline-block', 
 				'color':textColour,
 				'font-weight':'100',
+				'line-height':'12px',
 				'position':'fixed',
 				'z-index':'-100',
 				'top':'0px',
-				'left': 100*i
+				'left': 95*i
 			});		
 		}
-		result.sort();
-		$('.newDiv').html(result);	
+
+
+		// $('.newDiv').html(result);	
 	}
+	backgroundScroller(result); //, textColour);
+
+	function backgroundScroller(text){	
+		$('.newDiv').html(text);
+		setTimeout(function(){
+			backgroundScroller(text.substr(10) + text.substr(0, 10)); //, textColour);
+			console.log(text);
+			newResult = text;
+		},200);
+	}
+	backgroundScroller(newResult); 
+
+
 
 	//remove page after moving to next one. create array of remaining pages, choose one at random;
 
